63aae1a6634b35eaed839a34cd3d7351940ff359
Bul11k <139507357+SalimzianovBulat@users.noreply.github.com>
2024-05-19 22:46:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 469801461 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    import scipy.stats as sps
+    alpha = 0.03
+    return sps.ttest_ind(x, y)[1] <= alpha
+    
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
